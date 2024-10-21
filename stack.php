1b7b263c9ea7d10381bfab1f7308e3baf5e3b316
Bulana Bimsith <bulana2004@gmail.com>
2024-10-21 11:46:05 +0530

    10/20/2024 Class Work

--- stack.php
+++ stack.php
@@ -1,19 +1,49 @@
 <?php
 
-function dump($value){
-    echo "<pre>";
-    var_dump($value);
-    echo "</pre>";
+
+class Stack {
+	private $_size;
+	private $_stack = [];
+
+
+	public function __construct($size) {
+		$this->_size = $size;
+	}
+
+
+	public function push($n) {
+		if (count($this->_stack) >= $this->_size) {
+			return false;
+		}
+		$this->_stack[] = $n;
+		return true;
+	}
+
+
+	public function pop() {
+		return array_pop($this->_stack);
+	}
+
+
+	public function top() {
+		/**
+ 			:TODO implement this method
+		*/
+	}
 }
 
-class stack{
-    private $_size;
-    private $arr = [];
 
-    function __construct($size){
-        echo "stacks\n";
-        echo "test";
-    }
+/**
+* example usage
+*/
+$s1 = new Stack(20);
+
+// push values 1-20
+for ($i = 1; $i <= 20; ++$i) {
+	$s1->push($i);
 }
 
-new stack(15);
+// pop all values
+while (($val = $s1->pop()) != null) {
+	echo $val . "\n";
+}
